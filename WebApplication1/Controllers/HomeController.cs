be751249b646000c58b586b4bc7aea6bbeb9d840
github0424hy <github0424hy@gmail.com>
2019-12-28 12:37:24 +0900

    19/12/28

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description pageuss.";
+            ViewBag.Message = "Your application description pageussghghhg.";
 
             return View();
         }
